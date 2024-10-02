8a20cd8df101f83df6ab7c75ee7e44097ddc12c3
gin1221 <79311073+gin1221@users.noreply.github.com>
2024-10-02 19:32:16 +0700

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Thay đổi cho phù hợp với dự án của bạn
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
